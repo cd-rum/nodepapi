6dafd5b63b1224f8c1efc37a44af54886d81d5d4
Thomas Nash <null@endmail.xyz>
2019-12-04 09:26:58 +1100

    add proxy settings

--- press.js
+++ press.js
@@ -9,6 +9,8 @@ const Wordpress = require('wpapi')
 const app = express()
 
 app.listen(3000)
+app.set('trust proxy', true)
+app.use(express.logger(':req[X-Forwarded-For] - - [:date] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'))
 
 app.get('/', (req, res) => {
   res.json({ path: '/advant', status: 'okay' })
