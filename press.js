bf1ca9867fa1e55e7856a75d7745e40ab2471c09
Thomas Nash <null@endmail.xyz>
2019-12-03 15:46:13 +1100

    might be good

--- press.js
+++ press.js
@@ -9,7 +9,7 @@ const Wordpress = require('wpapi')
 const app = express()
 
 app.listen(3000)
-app.get('/posts/:id/:token', (req, res) => {
+app.get('/api/v1/posts/:id/:token', (req, res) => {
   http = axios.create({
     baseURL: `https://staging.advantplus.com.au/api/v4`,
     headers: { Authorization: `Bearer ${req.params['token']}` }
@@ -22,53 +22,63 @@ app.get('/posts/:id/:token', (req, res) => {
 const collectPost = (id) => {
   http.get(`/scheduled_posts/${id}`)
     .then(res => res.data.scheduled_post)
-    .then(data => {
-      const post = data
-      const api = buildApi(post)
-      const local = download(post.image_path)
+    .then(post => {
+      console.log(post)
 
-      // createPost(api, post)
-      // createMedia(api, local, post)
+      const api = buildApi(post)
+      const local = downloadImg(post.image_path)
+      const media = createMedia(api, local, post)
     })
     .catch(err => console.log(err))
 }
 
 const buildApi = (post) => {
-  const wpapi = new Wordpress({
+  const api = new Wordpress({
     endpoint: `${post.authorisation.page_id}/wp-json`,
     username: post.authorisation.name,
     password: post.authorisation.other_token
   })
-  return wpapi
+  return api
 }
 
-const createPost = (api, post) => {
+const createPost = (api, mediaId, post) => {
   api.posts()
     .create({
       title: post.document.title,
       content: post.document.content,
       excerpt: post.document.excerpt,
       comment_status: 'open',
-      status: 'draft'
+      status: post.draft ? 'draft' : 'publish',
+      featured_image_id: mediaId
     })
     .then(res => res)
     .catch(err => console.log(err))
 }
 
 const createMedia = (api, local, post) => {
-  api.media()
+  api.media().setHeaders('Content-Disposition', 'inline')
     .file(local)
-    .create({ title: post.document.title })
-    .then(res => res)
+    .create({
+      title: `Featured image for ${post.document.title}`
+    })
+    .then(media => {
+      console.log(media)
+      createPost(api, media.id)
+      return media
+    })
     .catch(err => console.log(err))
 }
 
 const downloadImg = (remote) => {
-  fetch(`https://dlsauy9pfhfq1.cloudfront.net/${remote}`)
-    .then(res => {
-      const dest = fs.createWriteStream(`./tmp/${path.basename(remote)}`)
-      res.body.pipe(dest)
-      return dest
-    })
-    .catch(err => console.log(err))
+  const filepath = `./tmp/${path.basename(remote)}`
+  if (fs.existsSync(filepath)) return filepath
+  else {
+    fetch(`https://dlsauy9pfhfq1.cloudfront.net/${remote}`)
+      .then(res => {
+        const dest = fs.createWriteStream(filepath)
+        res.body.pipe(dest)
+        return filepath
+      })
+      .catch(err => console.log(err))
+  }
 }
