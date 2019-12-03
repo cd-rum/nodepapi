5c6e0456b9b1ac42e72f2baf156fb8e006869522
Thomas Nash <null@endmail.xyz>
2019-12-04 10:42:42 +1100

    change some things

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # press-serve
-WPAPI is in Node, so we need a Node server.
+WPAPI is in Node so we need a Node server.
 
 ## Development
-Better logs.
-
-## Running
-Just starting Node.
+Better logs by default.
 
 `npm start`
 
 ## Testing
 Or really not since there are none.
 
-```
-npm test && http://0.0.0.0:3000/api/v1/posts/2143/a-token
-`
+`http://0.0.0.0:3000/adv/api/v1/posts/2143/a-token`
+
+## Production
+Move the service to `systemd` a la [these](https://stackoverflow.com/questions/4018154/how-do-i-run-a-node-js-app-as-a-background-service) instructions.
+
+`sudo systemctl restart press-serve.service`
--- press.js
+++ press.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 /* eslint no-console:0, no-unused-vars:0, no-undef:0 */
+// const cloud = 'ddwfsentcju8u'
+const cloud = 'dlsauy9pfhfq1'
+const host = 'staging.advantplus'
 
 const axios = require('axios')
 const express = require('express')
@@ -33,27 +36,29 @@ app.listen(3000)
 app.use(winstonExRegLogger.requestDetails)
 app.set('trust proxy', true)
 app.get('/adv', (req, res) => {
-  res.json({ path: '/advant', status: 'okay' })
+  res.json({ path: '/adv', status: 'okay' })
 })
 
-app.get('/adv/api/v1/posts/:id/:token', (req, res) => {
+app.post('/adv/api/v1/posts/:id/:token', (req, res) => {
   http = axios.create({
-    baseURL: `https://staging.advantplus.com.au/api/v4`,
-    headers: { Authorization: `Bearer ${req.params['token']}` }
+    baseURL: `https://${host}.com.au/api/v4`,
+    headers: { Authorization: `Bearer ${req.body.token}` }
   })
 
-  collectPost(req.params['id'])
-  res.json({ id: req.params['id'], token: req.params['token'] })
+  collectPost(req.body.id)
+  res.json({ id: req.body.id, token: req.body.token })
 })
 
 const collectPost = (id) => {
   http.get(`/scheduled_posts/${id}`)
     .then(res => res.data.scheduled_post)
     .then(post => {
+      console.log(post)
+
       const api = buildApi(post)
       const local = downloadImg(post.image_path)
-      const wordPost = createPost(api, local, post)
-      return wordPost
+      // const word = createMedia(api, local, post)
+      // return word
     })
     .catch(err => console.log(err))
 }
@@ -67,37 +72,30 @@ const buildApi = (post) => {
   return api
 }
 
-const createPost = (api, local, post) => {
+const createPost = (api, mediaId, post) => {
   api.posts()
     .create({
       title: post.document.title,
       content: post.document.content,
       excerpt: post.document.excerpt,
       comment_status: 'open',
-      status: post.draft ? 'draft' : 'publish'
+      status: post.draft ? 'draft' : 'publish',
+      featured_media_id: mediaId
     })
-    .then(res => {
-      const postId = res.id
-      createMedia(api, local, post, postId)
-      return res
-    })
+    .then(res => res)
     .catch(err => console.log(err))
 }
 
-const createMedia = (api, local, post, postId) => {
+const createMedia = (api, local, post) => {
   api.media().setHeaders('Content-Disposition', 'inline')
     .file(local)
     .create({
       title: `Featured image for ${post.document.title}`
     })
     .then(res => {
-      const imgId = res.id
-      return api.media().id(imgId).update({
-        post: postId
-      })
-    })
-    .then(res => {
-      console.log(`Media ${res.id} is now associated with post ${res.post}`)
+      const mediaId = res.id
+      createPost(api, mediaId, post)
+      return res
     })
     .catch(err => console.log(err))
 }
@@ -106,7 +104,7 @@ const downloadImg = (remote) => {
   const filepath = `./tmp/${path.basename(remote)}`
   if (fs.existsSync(filepath)) return filepath
   else {
-    fetch(`https://dlsauy9pfhfq1.cloudfront.net/${remote}`)
+    fetch(`https://${cloud}.cloudfront.net/${remote}`)
       .then(res => {
         const dest = fs.createWriteStream(filepath)
         res.body.pipe(dest)
