29abe866c8a70af7ef49af30b4607a4f19fb15c9
cd-rum <null@endmail.xyz>
2021-09-15 13:52:18 +1200

    mount actual file to sites-enabled

--- boot-pods.sh
+++ boot-pods.sh
@@ -2,4 +2,4 @@
 
 podman build . --tag=npapi
 podman run -v tmp:/app/tmp -p 3333:3000 npapi
-podman run -v ssl:/etc/ssl -v nginx:/etc/nginx -p 80:80 -p 443:443 nginx
+podman run -v ssl:/etc/ssl -v nginx/nginx.conf:/etc/nginx/sites-enabled/npapi.conf -p 80:80 -p 443:443 nginx
