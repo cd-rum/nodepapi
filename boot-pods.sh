1f32c97bc867622ecd72aee8c06488ca3789a3df
cd-rum <null@endmail.xyz>
2021-09-15 14:57:53 +1200

    whole path to conf

--- boot-pods.sh
+++ boot-pods.sh
@@ -2,4 +2,4 @@
 
 podman build . --tag=npapi
 podman run -v tmp:/app/tmp -p 3333:3333 npapi
-podman run -v ssl:/etc/ssl -v nginx:/etc/nginx -p 80:80 -p 443:443 nginx
+podman run -v ssl:/etc/ssl -v nginx/nginx.conf:/etc/nginx/nginx.conf -p 80:80 -p 443:443 nginx
