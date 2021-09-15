32674a58bcdbfc33cb419b62cda32f1bb650ff48
cd-rum <null@endmail.xyz>
2021-09-15 13:34:28 +1200

    change host port

--- boot-pods.sh
+++ boot-pods.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 podman build . --tag=npapi
-podman run -v tmp:/app/tmp -p 3000:3000 npapi
+podman run -v tmp:/app/tmp -p 3333:3000 npapi
 podman run -v ssl:/etc/ssl -v nginx:/etc/nginx -p 80:80 -p 443:443 nginx
