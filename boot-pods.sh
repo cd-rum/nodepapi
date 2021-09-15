3f8ace8fcc1b5d480093d976430821086351dd0a
cd-rum <null@endmail.xyz>
2021-09-15 14:09:09 +1200

    better ssl

--- boot-pods.sh
+++ boot-pods.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 podman build . --tag=npapi
-podman run -v tmp:/app/tmp -p 3333:3333 npapi
-podman run -v ssl:/etc/ssl -v nginx:/etc/nginx/sites-enabled -p 80:80 -p 443:443 nginx
+podman run -v tmp:/app/tmp -p 3333:33333000 npapi
+podman run -v ssl:/etc/ssl -v nginx:/etc/nginx -p 80:80 -p 443:443 nginx
